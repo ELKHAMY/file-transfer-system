a01b9459cbc3088f2751945094b684c0b4f2139d
Mostafa Elkhamy <81165303+ELKHAMY@users.noreply.github.com>
2021-07-18 21:46:01 +0200

    Add files via upload

---
+++ RmiInterface.java
@@ -0,0 +1,15 @@
+
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.rmi.*;
+import java.rmi.registry.*;
+public interface RmiInterface extends Remote {
+	
+	public void uploadFileToServer(byte[] mybyte, String serverpath, int length) throws RemoteException;
+	public byte[] downloadFileFromServer(String servername) throws RemoteException;
+	public String[] listFiles(String serverpath) throws RemoteException;
+	public boolean createDirectory(String serverpath) throws RemoteException;
+	public boolean removeDirectoryOrFile(String serverpath) throws RemoteException;
+
+}
